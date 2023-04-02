b455aed18aed8365deaebbd1ce87c6fbb5b0bc7e
soham <soham.piprotar2004@gmail.com>
2023-04-02 18:00:52 +0530

    just solve two binry tree problems and some basic introductiono of c++ :

---
+++ 1. Introduction of C++ Programming/introduction to while loop.cpp
@@ -0,0 +1,45 @@
+// introduction to while loop 
+
+#include <iostream>
+#include <vector>
+#include <stack>    
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <string> 
+#include <map>
+#include <set> 
+//#include <bits/stdc++.h>
+
+#define vi vector<int>
+#define vvi vector <vi> 
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first 
+#define ss second
+#define setbits(x) builtin_popcount(x)
+
+using namespace std;
+
+
+const int N =1e5+7 ;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int sum = 0  ;
+
+    while(n!= 0 ){
+
+    sum += n ;
+    n-- ;
+    }
+
+    cout<<sum<<endl;
+
+    return 0 ;
+}
